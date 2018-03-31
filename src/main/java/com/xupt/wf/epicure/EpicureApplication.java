ba7c8161950b5cee51a3a2bab9cadb6fda4350e5
wJupiter <wf_learner@163.com>
2018-03-31 12:34:47 +0800

    Inital commit

---
+++ src/main/java/com/xupt/wf/epicure/EpicureApplication.java
@@ -0,0 +1,12 @@
+package com.xupt.wf.epicure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicureApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xupt/wf/epicure/EpicureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xupt.wf.epicure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EpicureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
